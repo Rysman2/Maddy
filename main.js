980a42e521d89fa85fc449b9910d7f6f79c53aea
Rysman2 <100315359+Rysman2@users.noreply.github.com>
2025-03-24 00:32:39 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
-
 <script type="module">
-  // Import the functions you need from the SDKs you need
-  import { collection, getDocs } from 'https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js';
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-async function mostraRisultati() {
-  const querySnapshot = await getDocs(collection(db, "risposte"));
-  querySnapshot.forEach((doc) => {
-    // doc.id è l'ID del documento (generato automaticamente)
-    // doc.data() contiene i dati del documento
-    console.log(doc.id, " => ", doc.data());
-    // Puoi anche creare elementi HTML per mostrare i dati sul sito
-  });
-}
+  // Importa i moduli necessari da Firebase
+  import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js';
+  import { getAnalytics } from 'https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js';
+  import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js';
 
-mostraRisultati();
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // Configurazione del tuo progetto Firebase
   const firebaseConfig = {
     apiKey: "AIzaSyBSoaYo3Md4dXuqVV3cBfUuE3IbeY9QrTo",
     authDomain: "Maddy e DOmy.firebaseapp.com",
@@ -28,6 +14,26 @@ mostraRisultati();
     appId: "1:620926001920:web:a35dcf0ea6868cbbb649e5",
     measurementId: "G-ERQBZ8SGYV"
   };
+
+  // Inizializza Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  // Crea l'istanza di Firestore
+  const db = getFirestore(app);
+
+  // Funzione per mostrare i risultati dalla collezione "risposte"
+  async function mostraRisultati() {
+    const querySnapshot = await getDocs(collection(db, "risposte"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      // Qui puoi creare elementi HTML per mostrare i dati sul sito
+    });
+  }
+
+  // Chiama la funzione per mostrare i risultati
+  mostraRisultati();
+</script>
+
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
