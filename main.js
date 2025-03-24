1f166429e43f59d1da17982839bc8ed1e6aafa6a
Rysman2 <100315359+Rysman2@users.noreply.github.com>
2025-03-24 19:04:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,26 @@
 <script type="module">
-  // Importa i moduli necessari da Firebase
-  import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js';
-  import { getAnalytics } from 'https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js';
-  import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js';
-import { collection, addDoc } from 'firebase/firestore';
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-await addDoc(collection(db, "default"), {
-  nome: "Mario",
-  messaggio: "Ciao!"
-});
-  // Configurazione del tuo progetto Firebase
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyBSoaYo3Md4dXuqVV3cBfUuE3IbeY9QrTo",
-    authDomain: "MaddyeDOmy.firebaseapp.com",
-    projectId: "Maddy e DOmy",
-    storageBucket: "Maddy e DOmy.firebasestorage.app",
+    authDomain: "maddy-e-domy.firebaseapp.com",
+    projectId: "maddy-e-domy",
+    storageBucket: "maddy-e-domy.firebasestorage.app",
     messagingSenderId: "620926001920",
     appId: "1:620926001920:web:a35dcf0ea6868cbbb649e5",
     measurementId: "G-ERQBZ8SGYV"
   };
 
-  // Inizializza Firebase
+  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-  // Crea l'istanza di Firestore
-  const db = getFirestore(app);
-
-  // Funzione per mostrare i risultati dalla collezione "risposte"
-  async function mostraRisultati() {
-    const querySnapshot = await getDocs(collection(db, "default"));
-    console.log("Documenti trovati:", querySnapshot.size);
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-      // Qui puoi creare elementi HTML per mostrare i dati sul sit
-  });
-}
-
+</script>
   // Chiama la funzione per mostrare i risultati
   mostraRisultati();
 </script>
