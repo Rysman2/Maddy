e12c1fdfb4fd5734b7b38198719e7ab8fe69cb26
Rysman2 <100315359+Rysman2@users.noreply.github.com>
2025-03-24 22:16:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,10 @@
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
-
+<!-- Firebase App (the core Firebase SDK) -->
+<script src="https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js"></script>
+<!-- Add Firestore -->
+<script src="https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js"></script>
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
